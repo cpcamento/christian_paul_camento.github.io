709d32c2f808a1a011e817b953138be0b8fda8f2
cpcamento <75870556+cpcamento@users.noreply.github.com>
2020-12-16 12:56:01 +0800

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,12 +1,4 @@
-$('ul.menu li').click(function(){
-var id = $(this).attr('id');
-        if (id == "first") {
-            $('html,body').animate({
-                scrollTop: 0
-            }, 1000);
-        }else if(id == "second"){
-            $('html,body').animate({
-                scrollTop: 2000
-            }, 1000);
-        }
+$(document).ready(function(){
+
+alert('hello');
 });
