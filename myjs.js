aef12acaead39096c6d64951d062e535cd12e00f
cpcamento <75870556+cpcamento@users.noreply.github.com>
2020-12-16 10:58:55 +0800

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,6 +1,9 @@
 $('ul.menu li').click(function(){
-    var id = $(this).attr('id');
-   $("html, body").animate({
-        scrollTop: $('#'+id).offset().top 
-    }, 2000);
+var id = $(this).attr('id');
+  var target = $('#'+id);
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
 });
