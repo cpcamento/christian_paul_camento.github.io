ddfc8eef95e40bf0d23dea89c6b2d56a6d635606
cpcamento@gmail.com <cpcamento@gmail.com>
2023-09-04 16:48:52 +0800

    commit

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<?php
-
-  // Header
-  include_once('layouts/header.php');
-?>
+<?php include_once('layouts/header.php');?>
   
   <h1>CHRISTIAN PAUL CAMENTO</h1>
 
-<?php
-  // Footer
-  include_once('layouts/footer.php');
-
-?>
+<?php include_once('layouts/footer.php');?>
